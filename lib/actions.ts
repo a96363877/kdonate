66a5c948a9283c39d3652d1b20016a56349c7d27
a96363877 <a96363877@gmail.com>
2025-03-19 22:03:40 -0700

    as

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 
 
 export const playNotificationSound = () => {
-    const audio=new Audio('/not1.mp3')
+    const audio=new Audio('/not.wav')
     if (audio) {
       audio!.play().catch((error) => {
         console.error('Failed to play sound:', error);
